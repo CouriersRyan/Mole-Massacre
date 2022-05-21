4e80f592b2b9420ba637b35d84307beb7bf1dfd2
Kyle Zhang <kylezzhang@berkeley.edu>
2022-05-20 17:27:14 -0700

    added holder from other project (buggy)

---
+++ Assets/Scripts/Holder.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+///     Any object that, upon facing a Rigidbody, can pick up said Rigidbody
+/// </summary>
+public class Holder : MonoBehaviour
+{
+    /// <summary>
+    ///     Distance between position and holdable when held
+    /// </summary>
+    [SerializeField] private Vector3 holdOffset;
+
+    /// <summary>
+    ///     The rigidbody we are currently holding. Null if nothing is held.
+    /// </summary>
+    private Rigidbody held;
+
+    /// <summary>
+    ///     bruh
+    /// </summary>
+    private bool pickupPressed;
+
+    void FixedUpdate()
+    {
+        if (pickupPressed)
+        {
+            if (held)
+            {
+                held.transform.parent = null;
+                held.useGravity = true;
+                held = null;
+            }
+            else
+            {
+                var holdableMask = 1 << LayerMask.NameToLayer("Holdable");
+                var holderTransform = transform;
+                var holdPoint = holderTransform.position + holderTransform.TransformPoint(holdOffset);
+                var facingRay = new Ray(holdPoint, holderTransform.forward);
+                if (Physics.Raycast(facingRay, out var hitInfo, maxDistance: Mathf.Infinity, layerMask: holdableMask))
+                {
+                    held = hitInfo.rigidbody;
+                    held.transform.position = holdPoint;
+                    held.transform.parent = holderTransform;
+                    held.useGravity = false;
+                }
+            }
+        }
+
+        pickupPressed = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+                var holdPoint = transform.position + transform.TransformPoint(holdOffset);
+        Debug.DrawRay(holdPoint, 10f * transform.forward, Color.green);
+    }
+
+
+    void OnPickUp(InputValue inputValue)
+    {
+        pickupPressed = inputValue.isPressed;
+    }
+}
