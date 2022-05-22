e69ee284d3fa0f7703b6b333f353d5937aa0a4cb
Kyle Zhang <kylezzhang@berkeley.edu>
2022-05-21 23:44:36 -0700

    restart & limited lives
    There are a lot of TODOs now

--- Assets/Scripts/Player/PlayerMole.cs
+++ Assets/Scripts/Player/PlayerMole.cs
@@ -6,18 +6,23 @@ using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 // Handles dying, respawning, and ending the level.
+// TODO: Maybe separate level logic (ending/restarting level) from dying/corpse logic?
 public class PlayerMole : MonoBehaviour
 {
     [SerializeField] private GameObject corpse;
     [SerializeField] private Vector3 respawnPoint;
+    // TODO: Retrieve max lives from level data somewhere instead of from inspector
+    [SerializeField] private int maxLives;
 
     private ThirdPersonController _controller;
     private CharacterController _characterController;
+    private int _lives;
 
-    private void Start()
+    private void Awake()
     {
         _controller = GetComponent<ThirdPersonController>();
         _characterController = GetComponent<CharacterController>();
+        _lives = maxLives;
     }
 
     // Run when the player dies.
@@ -25,6 +30,19 @@ public class PlayerMole : MonoBehaviour
     {
         Instantiate(corpse, transform.position, transform.rotation);
         _controller.Teleport(respawnPoint, Quaternion.identity);
+        _lives--;
+        if (_lives <= 0)
+        {
+            // TODO: Add an actual game over screen (maybe allow player to restart)
+            Debug.Log("Game over!");
+#if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -39,7 +57,7 @@ public class PlayerMole : MonoBehaviour
                 respawnPoint = other.transform.position;
                 break;
             case "Finish":
-                Debug.Log("Level End!"); 
+                Debug.Log("Level End!");
                 // TODO: Call an event from Game Manager to open the end of level menu.
                 break;
         }
@@ -55,7 +73,7 @@ public class PlayerMole : MonoBehaviour
                 break;
         }
     }
-    
+
     // TODO: Move this to some UI button and/or indicate hotkey somewhere in the game
     /// <summary>
     ///     Reloads the active scene.
